a6f6b09777a84c63c736c6c7277548315d7c8b46
wertrain <wertrain@gmail.com>
2021-05-27 23:38:41 +0900

    動的輪郭抽出の準備

---
+++ PiecePathMaker/PiecePathMakerLib/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiecePathMakerLib
+{
+    /// <summary>
+    /// 動的輪郭抽出（Snake algorithm）
+    /// 
+    /// https://qiita.com/kinketsucom/items/3d48f8705cde44d88eaf
+    /// https://kyudy.hatenablog.com/entry/2020/05/31/104245
+    /// </summary>
+    class Snake
+    {
+    }
+}
