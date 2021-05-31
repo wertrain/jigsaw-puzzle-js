1d733390b66b87b725d0da29ecb897dd57bc94a6
wertrain <wertrain@gmail.com>
2021-05-31 23:42:26 +0900

    OpenCV の snake によるテスト実装

--- PiecePathMaker/PiecePathMakerLib/PiecePathMaker.cs
+++ PiecePathMaker/PiecePathMakerLib/PiecePathMaker.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 
@@ -27,9 +29,39 @@ namespace PiecePathMakerLib
         /// </summary>
         /// <param name="fileName"></param>
         /// <returns></returns>
-        public List<Point> MakePointsFromFile(string fileName)
+        public static List<Point> MakePointsFromFile(string fileName)
         {
             var points = new List<Point>();
+
+            IplImage src = new IplImage(fileName, LoadMode.GrayScale);
+            IplImage dst = new IplImage(src.Size, BitDepth.U8, 3);
+
+            CvPoint[] contour = new CvPoint[100];
+
+            CvPoint center = new CvPoint(src.Width / 2, src.Height / 2);
+            for (int i = 0; i < contour.Length; i++)
+            {
+                contour[i].X = (int)(center.X * Math.Cos(2 * Math.PI * i / contour.Length) + center.X);
+                contour[i].Y = (int)(center.Y * Math.Sin(2 * Math.PI * i / contour.Length) + center.Y);
+            }
+
+            CvWindow w = new CvWindow();
+            while (true)
+            {
+                src.SnakeImage(contour, 0.8f, 0.0f, 0.3f, new CvSize(15, 15), new CvTermCriteria(1), true);
+                src.CvtColor(dst, ColorConversion.GrayToRgb);
+                for (int i = 0; i < contour.Length - 1; i++)
+                {
+                    dst.Line(contour[i], contour[i + 1], new CvColor(255, 0, 0), 2);
+                }
+                dst.Line(contour[contour.Length - 1], contour[0], new CvColor(255, 0, 0), 2);
+                w.Image = dst;
+                int key = CvWindow.WaitKey();
+                if (key == 27)
+                {
+                    break;
+                }
+            }
 
             return points;
         }
