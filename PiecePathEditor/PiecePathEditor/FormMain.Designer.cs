4269d52be5f59b2db2e6bacaa3c4c761d2209db6
wertrain <wertrain@gmail.com>
2021-06-04 23:57:02 +0900

    WIP

--- PiecePathEditor/PiecePathEditor/FormMain.Designer.cs
+++ PiecePathEditor/PiecePathEditor/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PiecePathEditor
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 2;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.2398F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.7602F));
+            this.tableLayoutPanelMain.Controls.Add(this.pictureBoxCanvas, 0, 0);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 1;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // pictureBoxCanvas
+            // 
+            this.pictureBoxCanvas.BackColor = System.Drawing.Color.White;
+            this.pictureBoxCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxCanvas.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxCanvas.Name = "pictureBoxCanvas";
+            this.pictureBoxCanvas.Size = new System.Drawing.Size(529, 555);
+            this.pictureBoxCanvas.TabIndex = 0;
+            this.pictureBoxCanvas.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.Name = "FormMain";
+            this.Text = "PathEditor";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.PictureBox pictureBoxCanvas;
     }
 }
 
