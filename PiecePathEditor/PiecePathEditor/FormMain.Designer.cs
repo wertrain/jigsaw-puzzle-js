0889ffe8d9c001a71eab7589450380d0a22ff41d
wertrain <wertrain@gmail.com>
2021-06-05 21:13:46 +0900

    WIP

--- PiecePathEditor/PiecePathEditor/FormMain.Designer.cs
+++ PiecePathEditor/PiecePathEditor/FormMain.Designer.cs
@@ -28,54 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
-            this.tableLayoutPanelMain.SuspendLayout();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.listViewPoints = new System.Windows.Forms.ListView();
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPoint = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemOpenImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanelOutput = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
+            this.splitContainerMain.Panel1.SuspendLayout();
+            this.splitContainerMain.Panel2.SuspendLayout();
+            this.splitContainerMain.SuspendLayout();
+            this.tableLayoutPanelOutput.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanelMain
-            // 
-            this.tableLayoutPanelMain.ColumnCount = 2;
-            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.2398F));
-            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.7602F));
-            this.tableLayoutPanelMain.Controls.Add(this.pictureBoxCanvas, 0, 0);
-            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
-            this.tableLayoutPanelMain.RowCount = 1;
-            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelMain.Size = new System.Drawing.Size(784, 561);
-            this.tableLayoutPanelMain.TabIndex = 0;
             // 
             // pictureBoxCanvas
             // 
             this.pictureBoxCanvas.BackColor = System.Drawing.Color.White;
             this.pictureBoxCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxCanvas.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxCanvas.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxCanvas.Name = "pictureBoxCanvas";
-            this.pictureBoxCanvas.Size = new System.Drawing.Size(529, 555);
+            this.pictureBoxCanvas.Size = new System.Drawing.Size(318, 355);
             this.pictureBoxCanvas.TabIndex = 0;
             this.pictureBoxCanvas.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(511, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "File(&F)";
+            // 
+            // listViewPoints
+            // 
+            this.listViewPoints.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderPoint});
+            this.listViewPoints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewPoints.HideSelection = false;
+            this.listViewPoints.Location = new System.Drawing.Point(3, 3);
+            this.listViewPoints.Name = "listViewPoints";
+            this.listViewPoints.Size = new System.Drawing.Size(183, 175);
+            this.listViewPoints.TabIndex = 1;
+            this.listViewPoints.UseCompatibleStateImageBehavior = false;
+            this.listViewPoints.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Name";
+            this.columnHeaderName.Width = 77;
+            // 
+            // columnHeaderPoint
+            // 
+            this.columnHeaderPoint.Text = "Point";
+            this.columnHeaderPoint.Width = 56;
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemOpenImage});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(51, 20);
+            this.toolStripMenuItemFile.Text = "File(&F)";
+            // 
+            // toolStripMenuItemOpenImage
+            // 
+            this.toolStripMenuItemOpenImage.Name = "toolStripMenuItemOpenImage";
+            this.toolStripMenuItemOpenImage.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemOpenImage.Text = "Open Image(&O)";
+            // 
+            // splitContainerMain
+            // 
+            this.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMain.Location = new System.Drawing.Point(0, 24);
+            this.splitContainerMain.Name = "splitContainerMain";
+            // 
+            // splitContainerMain.Panel1
+            // 
+            this.splitContainerMain.Panel1.Controls.Add(this.pictureBoxCanvas);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainerMain.Panel2.Controls.Add(this.tableLayoutPanelOutput);
+            this.splitContainerMain.Size = new System.Drawing.Size(511, 355);
+            this.splitContainerMain.SplitterDistance = 318;
+            this.splitContainerMain.TabIndex = 2;
+            // 
+            // tableLayoutPanelOutput
+            // 
+            this.tableLayoutPanelOutput.ColumnCount = 1;
+            this.tableLayoutPanelOutput.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelOutput.Controls.Add(this.listViewPoints, 0, 0);
+            this.tableLayoutPanelOutput.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanelOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelOutput.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelOutput.Name = "tableLayoutPanelOutput";
+            this.tableLayoutPanelOutput.RowCount = 2;
+            this.tableLayoutPanelOutput.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.98592F));
+            this.tableLayoutPanelOutput.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.01408F));
+            this.tableLayoutPanelOutput.Size = new System.Drawing.Size(189, 355);
+            this.tableLayoutPanelOutput.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 184);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 168);
+            this.textBox1.TabIndex = 2;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.tableLayoutPanelMain);
+            this.ClientSize = new System.Drawing.Size(511, 379);
+            this.Controls.Add(this.splitContainerMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormMain";
             this.Text = "PathEditor";
-            this.tableLayoutPanelMain.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainerMain.Panel1.ResumeLayout(false);
+            this.splitContainerMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
+            this.splitContainerMain.ResumeLayout(false);
+            this.tableLayoutPanelOutput.ResumeLayout(false);
+            this.tableLayoutPanelOutput.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
         private System.Windows.Forms.PictureBox pictureBoxCanvas;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpenImage;
+        private System.Windows.Forms.ListView listViewPoints;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderPoint;
+        private System.Windows.Forms.SplitContainer splitContainerMain;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelOutput;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
