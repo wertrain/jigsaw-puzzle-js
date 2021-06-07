342b2b8dfa8e0392d0ce53d968725f930ec7fddb
wertrain <wertrain@gmail.com>
2021-06-07 23:41:00 +0900

    WIP

---
+++ PiecePathEditor/PiecePathEditor/CommandManager.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiecePathEditor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CommandManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CommandManager Instance { get; } = new CommandManager();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class Point
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            public int X { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public int Y { get; set; }
+
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public enum CommandTypes
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            None,
+
+            /// <summary>
+            /// 
+            /// </summary>
+            Add,
+
+            /// <summary>
+            /// 
+            /// </summary>
+            Move,
+
+            /// <summary>
+            /// 
+            /// </summary>
+            Remove,
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class Command
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            public CommandTypes Type { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public Point Point { get; set; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Queue<Command> CommandQueue;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private CommandManager()
+        {
+
+        }
+    }
+}
