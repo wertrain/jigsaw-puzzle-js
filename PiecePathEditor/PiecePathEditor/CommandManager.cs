f31b4da6363103b1a7205fa1eb1c015e84bb2ebd
wertrain <wertrain@gmail.com>
2021-06-09 02:00:02 +0900

    WIP

--- PiecePathEditor/PiecePathEditor/CommandManager.cs
+++ PiecePathEditor/PiecePathEditor/CommandManager.cs
@@ -9,33 +9,6 @@ namespace PiecePathEditor
     /// <summary>
     /// 
     /// </summary>
-    public class Point
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public int X { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public int Y { get; set; }
-
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
     public class CommandManager
     {
         /// <summary>
--- PiecePathEditor/PiecePathEditor/Commands.cs
+++ PiecePathEditor/PiecePathEditor/Commands.cs
@@ -7,7 +7,34 @@ using System.Threading.Tasks;
 namespace PiecePathEditor
 {
     /// <summary>
-    /// 
+    /// 座標
+    /// </summary>
+    public class Point
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    /// <summary>
+    /// コマンドの基底クラス
     /// </summary>
     public class CommandBase
     {
@@ -15,9 +42,17 @@ namespace PiecePathEditor
     }
 
     /// <summary>
-    /// 
+    /// ポイント追加コマンド
     /// </summary>
     public class CommandAddPoint : CommandBase
+    {
+
+    }
+
+    /// <summary>
+    /// ポイント移動コマンド
+    /// </summary>
+    public class CommandMovePoint :CommandBase
     {
 
     }
